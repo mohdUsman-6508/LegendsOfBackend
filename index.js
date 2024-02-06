bb51782a1657640dbd3dd8ad9065f4c13a16be56
Mohammad Usman <uk351675@gmail.com>
2024-02-06 23:40:50 +0530

    express.js basic

--- index.js
+++ index.js
@@ -10,30 +10,50 @@
 
 //another way of importing
 
-import http from "http";
-import greet from "./aModule.js";
-import { sayHello, goodbye } from "./aModule.js";
-import fs from "fs"; //file reading ke kaam aata he
+// import http from "http";
+// import greet from "./aModule.js";
+// import { sayHello, goodbye } from "./aModule.js";
+// import fs from "fs"; //file reading ke kaam aata he
+// import path from "path";
+
+// fs.readFile("./index.htm", () => {
+//   console.log("file read");
+// }); //asyn task
+
+// console.log(path.extname("a.js"));
+// const home = fs.readFileSync("./index.htm");
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.method);
+//   if (req.url === "/home") res.end(home);
+//   else if (req.url === "/about") res.end("About");
+//   else res.end(`<h1>${goodbye()}</h1>`);
+// });
+
+// server.listen(3000, () => {
+//   console.log("server is running...");
+// });
+
+// console.log(greet());
+// console.log(goodbye());
+// console.log("hello");
+///
+import express from "express";
 import path from "path";
+const app = express();
 
-fs.readFile("./index.htm", () => {
-  console.log("file read");
-}); //asyn task
-
-console.log(path.extname("a.js"));
-const home = fs.readFileSync("./index.htm");
-
-const server = http.createServer((req, res) => {
-  console.log(req.method);
-  if (req.url === "/home") res.end(home);
-  else if (req.url === "/about") res.end("About");
-  else res.end(`<h1>${goodbye()}</h1>`);
+app.get("/", (req, res) => {
+  // res.send("Hi,Welcome");
+  // res.sendStatus(400);
+  const pathLocation = path.resolve();
+  res.sendFile(path.join(pathLocation, "./index.htm"));
+  // res.json({
+  //   success: true,
+  //   confidence: true,
+  //   rank: [1, 1, 1],
+  // });
 });
 
-server.listen(3000, () => {
-  console.log("server is running...");
+app.listen(5000, () => {
+  console.log("Listening at port 5000");
 });
-
-console.log(greet());
-console.log(goodbye());
-console.log("hello");
