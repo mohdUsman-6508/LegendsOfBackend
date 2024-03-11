a75193ea0ec2444a4e776fd9836ed0f5345aab66
Mohammad Usman <uk351675@gmail.com>
2024-03-12 00:44:48 +0530

    student update api

--- student/app.js
+++ student/app.js
@@ -40,6 +40,7 @@ app.get("/", (req, res) => {
 });
 
 // Handle form submission
+//create student
 app.post("/new", async (req, res) => {
   let { name, faculty_no, enrollment_no, course } = req.body;
 
@@ -53,15 +54,47 @@ app.post("/new", async (req, res) => {
         course,
       });
       console.log(student);
+      res.status(200).json({
+        success: true,
+        student,
+      });
       res.redirect("/");
     } catch (err) {
       console.log(err);
       res.status(500).send("Error occurred while creating student.");
     }
   } else {
+    res.status(404).json({
+      success: false,
+      message: "Student already enrolled!",
+    });
     console.log("student exits");
     res.render("alreadyExist");
   }
+});
+
+//update student credentials
+
+app.put("/update", async (req, res) => {
+  let { name, faculty_no, enrollment_no, course } = req.body;
+  let exist = await Student.findOne({ enrollment_no });
+
+  if (exist) {
+    let updated = await Student.updateOne({
+      name,
+      faculty_no,
+      course,
+    });
+
+    res.status(200).json({
+      success: true,
+      updated,
+    });
+  } else
+    res.status(404).json({
+      success: false,
+      message: "not updated!",
+    });
 });
 
 // Start the server
